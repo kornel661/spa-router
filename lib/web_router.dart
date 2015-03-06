896900506658e24099fee6be4727f1aad90770da
Kornel Maczyński <kornel661@gmail.com>
2015-03-06 23:27:58 +0000

    fix?: template binding

--- lib/web_router.dart
+++ lib/web_router.dart
@@ -9,6 +9,7 @@ import 'dart:async';
 import 'package:core_elements/core_animated_pages.dart';
 import 'package:web_router/web_route.dart';
 import 'package:core_elements/core_ajax_dart.dart';
+import 'package:template_binding/template_binding.dart';
 
 Map importedURIs = {};
 RouteUri previousUrl = new RouteUri.parse(window.location.href, "auto");
@@ -31,7 +32,7 @@ class WebRouter extends PolymerElement {
 	@published String trailingSlash = "strict";
 	@published bool shadow = false;
 	/// typecast="auto|string"
-	/// If string then even 123 will be passed as a string '123'.
+	/// If string then even 123 will be passed as a string '123'?
 	@published String typecast = "auto";
 	/// Whether to use Polymer's core-animated-pages for transitions.
 	@published bool core_animated_pages = false;
@@ -440,19 +441,20 @@ void activeTemplate(WebRouter router, TemplateElement template, WebRoute route, 
 	//TODO: inline template and its binding seems not to be working always yet, for example when app-router itself is contained in a (auto-binding) template.
 	
 	Map<String, String> model = createModel(router, route, url, eventDetail);
-	if (model != {}) {//Has to be auto-binding template then
-		//// template.createInstance(model) is a Polymer method that binds a model to a template and also fixes
-		//// https://github.com/erikringsmuth/app-router/issues/19
-		//print("Using auto-binding template in app-router.");
-		templateInstance = (template as AutoBindingElement).createInstance(model);//TODO: Not working yet
-		////templateInstance = templateBindFallback(template).createInstance(model: model);
-		////template.model = toObservable(model);//
-		////templateBind(template).model = toObservable(model);
-		////templateInstance = template;
-	} else {
-		templateInstance = document.importNode(template.content, true);
-	}
-	
+//	if (model != {}) {//Has to be auto-binding template then
+//		//// template.createInstance(model) is a Polymer method that binds a model to a template and also fixes
+//		//// https://github.com/erikringsmuth/app-router/issues/19
+//		//print("Using auto-binding template in app-router.");
+//		templateInstance = (template as AutoBindingElement).createInstance(model);//TODO: Not working yet
+//		////templateInstance = templateBindFallback(template).createInstance(model: model);
+//		////template.model = toObservable(model);//
+//		////templateBind(template).model = toObservable(model);
+//		////templateInstance = template;
+//	} else {
+//		templateInstance = document.importNode(template.content, true);
+//	}
+	//FIXME(km): check if it works
+	templateInstance = templateBind(template).createInstance(model);
 	activeElement(router, templateInstance, url, eventDetail);
 }
 
@@ -487,232 +489,232 @@ void activeElement(WebRouter router, Node element, RouteUri url,
 	
 	// animate the transition if core-animated-pages are being used
 	if (router.core_animated_pages) {
-	  router.coreAnimatedPages.selected = router.activeRoute.path;
-	
-	  // we already wired up transitionAnimationEnd() in init()
-	
-	  // use to check if the previous route has finished animating before being removed
-	  if (router.previousRoute != null) {
-	    router.previousRoute.transitionAnimationInProgress = true;
-	  }
+		router.coreAnimatedPages.selected = router.activeRoute.path;
+		
+		// we already wired up transitionAnimationEnd() in init()
+		
+		// use to check if the previous route has finished animating before being removed
+		if (router.previousRoute != null) {
+			router.previousRoute.transitionAnimationInProgress = true;
+		}
 	}
 	
 	// scroll to the URL hash if it's present
 	if (url.hash != null && !router.core_animated_pages) {
-	  scrollToHash(url.hash);
+		scrollToHash(url.hash);
 	}
 	
 	router.fireEvent('activate-route-end', eventDetail, router);
 	router.fireEvent('activate-route-end', eventDetail, eventDetail['route']);
 }
 
-  // Call when the previousRoute has finished the transition animation out
+/// Call when the previousRoute has finished the transition animation out.
 void transitionAnimationEnd(WebRoute previousRoute) {
-  if (previousRoute != null) {
-    previousRoute.transitionAnimationInProgress = false;
-    removeRouteContent(previousRoute);
-  }
+	if (previousRoute != null) {
+		previousRoute.transitionAnimationInProgress = false;
+		removeRouteContent(previousRoute);
+	}
 }
 
-  // Remove the route's content (but not the <template> if it exists)
+/// Remove the route's content (but not the <template> if it exists).
 void removeRouteContent(WebRoute route) {
-  if (route != null) {
-    List<Element> newChildren = [];
-    for (Element node in route.children) {
-      if (node.tagName == 'TEMPLATE') {
-        newChildren.add(node);
-      }
-    }
-    route.children = newChildren;
-  }
+	if (route != null) {
+		List<Element> newChildren = [];
+		for (Element node in route.children) {
+			if (node is TemplateElement) {
+			//if (node.tagName == 'TEMPLATE') {
+				newChildren.add(node);
+			}
+		}
+		route.children = newChildren;
+	}
 }
 
-  // scroll to the element with id="hash" or name="hash"
+/// Scroll to the element with id="hash" or name="hash".
 void scrollToHash(String hash) {
-  if (hash == null || hash == '') return;
-
-  // wait for the browser's scrolling to finish before we scroll to the hash
-  // ex: http://example.com/#/page1#middle
-  // the browser will scroll to an element with id or name `/page1#middle` when the page finishes loading. if it doesn't exist
-  // it will scroll to the top of the page. let the browser finish the current event loop and scroll to the top of the page
-  // before we scroll to the element with id or name `middle`.
-
-  void onTimerScrollToHash () {
-    Element hashElement = document.querySelector('html /deep/ ' + hash);
-    if (hashElement == null) {
-      hashElement = document.querySelector('html /deep/ [name="' + hash.substring(1) + '"]');
-    }
-    if (hashElement != null /*&& hashElement.scrollIntoView*/) {//TODO
-      hashElement.scrollIntoView(ScrollAlignment.TOP);
-    }
-  }
-
-  new Timer(new Duration(milliseconds: 0), onTimerScrollToHash);//TODO
+	if (hash == null || hash == '') return;
+	
+	// wait for the browser's scrolling to finish before we scroll to the hash
+	// ex: http://example.com/#/page1#middle
+	// the browser will scroll to an element with id or name `/page1#middle` when the page finishes loading. if it doesn't exist
+	// it will scroll to the top of the page. let the browser finish the current event loop and scroll to the top of the page
+	// before we scroll to the element with id or name `middle`.
+	
+	void onTimerScrollToHash () {
+		Element hashElement = document.querySelector('html /deep/ ' + hash);
+		if (hashElement == null) {
+			hashElement = document.querySelector('html /deep/ [name="' + hash.substring(1) + '"]');
+		}
+		if (hashElement != null /*&& hashElement.scrollIntoView*/) {//TODO
+			hashElement.scrollIntoView(ScrollAlignment.TOP);
+		}
+	}
+	
+	new Timer(new Duration(milliseconds: 0), onTimerScrollToHash);//TODO
 }
 
-  // testRoute(routePath, urlPath, trailingSlashOption, isRegExp) - Test if the route's path matches the URL's path
-  //
-  // Example routePath: '/example/*'
-  // Example urlPath = '/example/path'
+/// testRoute(routePath, urlPath, trailingSlashOption, isRegExp) - Test if the route's path matches the URL's path
+///
+/// Example routePath: '/example/*'
+/// Example urlPath = '/example/path'
 bool testRoute(String routePath, String urlPath, String trailingSlashOption, bool isRegExp) {
-  // this algorithm tries to fail or succeed as quickly as possible for the most common cases
-
-  // handle trailing slashes (options: strict (default), ignore)
-  if (trailingSlashOption == 'ignore') {
-    // remove trailing / from the route path and URL path
-    if(urlPath.endsWith('/')) {
-      urlPath = urlPath.substring(0, urlPath.length-1);//TODO
-    }
-    if(routePath.endsWith('/') && !isRegExp) {
-      routePath = routePath.substring(0, routePath.length-1);
-    }
-  }
-
-  // test regular expressions
-  if (isRegExp) {
-    return testRegExString(routePath, urlPath);
-  }
-
-  // if the urlPath is an exact match or '*' then the route is a match
-  if (routePath == urlPath || routePath == '*') {
-    return true;
-  }
-
-  // look for wildcards
-  if (routePath.indexOf('*') == -1 && routePath.indexOf(':') == -1) {
-    // no wildcards and we already made sure it wasn't an exact match so the test fails
-    return false;
-  }
-
-  // example urlPathSegments = ['', example', 'path']
-  List<String> urlPathSegments = urlPath.split('/');
-
-  // example routePathSegments = ['', 'example', '*']
-  List<String> routePathSegments = routePath.split('/');
-
-  // there must be the same number of path segments or it isn't a match
-  if (urlPathSegments.length != routePathSegments.length) {
-    return false;
-  }
-
-  // check equality of each path segment
-  for (int i = 0; i < routePathSegments.length; i++) {
-    // the path segments must be equal, be a wildcard segment '*', or be a path parameter like ':id'
-    String routeSegment = routePathSegments[i];
-    if (routeSegment != urlPathSegments[i] && routeSegment != '*' && !routeSegment.startsWith(':')) {
-      // the path segment wasn't the same string and it wasn't a wildcard or parameter
-      return false;
-    }
-  }
-
-  // nothing failed. the route matches the URL.
-  return true;
+	// this algorithm tries to fail or succeed as quickly as possible for the most common cases
+	
+	// handle trailing slashes (options: strict (default), ignore)
+	if (trailingSlashOption == 'ignore') {
+		// remove trailing / from the route path and URL path
+		if (urlPath.endsWith('/')) {
+			urlPath = urlPath.substring(0, urlPath.length-1);//TODO
+		}
+		if (routePath.endsWith('/') && !isRegExp) {
+			routePath = routePath.substring(0, routePath.length-1);
+		}
+	}
+	
+	// test regular expressions
+	if (isRegExp) {
+		return testRegExString(routePath, urlPath);
+	}
+	
+	// if the urlPath is an exact match or '*' then the route is a match
+	if (routePath == urlPath || routePath == '*') {
+		return true;
+	}
+	
+	// look for wildcards
+	if (routePath.indexOf('*') == -1 && routePath.indexOf(':') == -1) {
+	  // no wildcards and we already made sure it wasn't an exact match so the test fails
+	  return false;
+	}
+	
+	// example urlPathSegments = ['', example', 'path']
+	List<String> urlPathSegments = urlPath.split('/');
+	
+	// example routePathSegments = ['', 'example', '*']
+	List<String> routePathSegments = routePath.split('/');
+	
+	// there must be the same number of path segments or it isn't a match
+	if (urlPathSegments.length != routePathSegments.length) {
+		return false;
+	}
+	
+	// check equality of each path segment
+	for (int i = 0; i < routePathSegments.length; i++) {
+	  // the path segments must be equal, be a wildcard segment '*', or be a path parameter like ':id'
+	  String routeSegment = routePathSegments[i];
+	  if (routeSegment != urlPathSegments[i] && routeSegment != '*' && !routeSegment.startsWith(':')) {
+	    // the path segment wasn't the same string and it wasn't a wildcard or parameter
+	    return false;
+	  }
+	}
+	
+	// nothing failed. the route matches the URL.
+	return true;
 }
 
-  // routeArguments(routePath, urlPath, search, isRegExp) - Gets the path variables and query parameter values from the URL
+/// routeArguments(routePath, urlPath, search, isRegExp) - Gets the path variables and query parameter values from the URL.
 Map routeArguments(String routePath, String urlPath, String search, bool isRegExp, bool autoTypecast) {
-  Map<String, String> args = {};
-
-  // regular expressions can't have path variables
-  if (!isRegExp) {
-    // example urlPathSegments = ['', example', 'path']
-    List<String> urlPathSegments = urlPath.split('/');
-
-    // example routePathSegments = ['', 'example', '*']
-    List<String> routePathSegments = routePath.split('/');
-
-    // get path variables
-    // urlPath '/customer/123'
-    // routePath '/customer/:id'
-    // parses id = '123'
-    for (int index = 0; index < routePathSegments.length; index++) {
-      String routeSegment = routePathSegments[index];
-      if (routeSegment.startsWith(':')) {
-        args[routeSegment.substring(1)] = urlPathSegments[index];
-      }
-    }
-  }
-
-  List<String> queryParameters = [];
-  if (search.length>0){
-    queryParameters = search.substring(1).split('&');
-  }
-  // split() on an empty string has a strange behavior of returning [''] instead of []
-  if (queryParameters.length == 1 && queryParameters[0] == '') {
-    queryParameters = [];
-  }
-  for (int i = 0; i < queryParameters.length; i++) {
-    String queryParameter = queryParameters[i];
-    List<String> queryParameterParts = queryParameter.split('=');
-    args[queryParameterParts[0]] = queryParameterParts.sublist(1).join('=');//TODO
-  }
-
-  if (autoTypecast) {
-    // parse the arguments into unescaped strings, numbers, or booleans
-    for (String arg in args.keys) {
-      args[arg] = typecast(args[arg]);
-    }
-  }
-
-  return args;
+	Map<String, String> args = {};
+	
+	// regular expressions can't have path variables
+	if (!isRegExp) {
+		// example urlPathSegments = ['', example', 'path']
+		List<String> urlPathSegments = urlPath.split('/');
+		
+		// example routePathSegments = ['', 'example', '*']
+		List<String> routePathSegments = routePath.split('/');
+		
+		// get path variables
+		// urlPath '/customer/123'
+		// routePath '/customer/:id'
+		// parses id = '123'
+		for (int index = 0; index < routePathSegments.length; index++) {
+			String routeSegment = routePathSegments[index];
+			if (routeSegment.startsWith(':')) {
+				args[routeSegment.substring(1)] = urlPathSegments[index];
+			}
+		}
+	}
+	
+	List<String> queryParameters = [];
+	if (search.length > 0) {
+	  queryParameters = search.substring(1).split('&');
+	}
+	// split() on an empty string has a strange behavior of returning [''] instead of []
+	if (queryParameters.length == 1 && queryParameters[0] == '') {
+		queryParameters = [];
+	}
+	for (int i = 0; i < queryParameters.length; i++) {
+		String queryParameter = queryParameters[i];
+		List<String> queryParameterParts = queryParameter.split('=');
+		args[queryParameterParts[0]] = queryParameterParts.sublist(1).join('=');//TODO
+	}
+	
+	if (autoTypecast) {
+		// parse the arguments into unescaped strings, numbers, or booleans
+		for (String arg in args.keys) {
+			args[arg] = typecast(args[arg]);
+		}
+	}
+	
+	return args;
 }
 
-  // typecast(value) - Typecast the string value to an unescaped string, number, or boolean
-String typecast (String value) {
-  // bool
-  if (value == 'true') {
-    return 'true';
-  }
-  if (value == 'false') {
-    return 'false';
-  }
-
-  // number
-  if (value != '' && !value.startsWith('0')) {
-    int number = int.parse(value, onError: (string) => 0);
-    if (number != 0) {
-      return number.toString();
-    }
-  }
-
-  // string
-  return Uri.decodeComponent(value);
+/// typecast(value) - Typecast the string value to an unescaped string, number, or boolean.
+String typecast(String value) {
+	// bool
+	if (value == 'true') {
+		return 'true';
+	}
+	if (value == 'false') {
+		return 'false';
+	}
+	
+	// number
+	if (value != '' && !value.startsWith('0')) {
+		int number = int.parse(value, onError: (string) => 0);
+		if (number != 0) {
+			return number.toString();
+		}
+	}
+	
+	// string
+	return Uri.decodeComponent(value);
 }
 
-  // testRegExString(pattern, value) - Parse HTML attribute path="/^\/\w+\/\d+$/i" to a regular
-  // expression `new RegExp('^\/\w+\/\d+$', 'i')` and test against it.
-  //
-  // note that 'i' is the only valid option. global 'g', multiline 'm', and sticky 'y' won't be valid matchers for a path.
+/// testRegExString(pattern, value) - Parse HTML attribute path="/^\/\w+\/\d+$/i" to a regular
+/// expression `new RegExp('^\/\w+\/\d+$', 'i')` and test against it.
+///
+/// note that 'i' is the only valid option. global 'g', multiline 'm', and sticky 'y' won't be valid matchers for a path.
 bool testRegExString(String pattern, String value) {
-  if (!pattern.startsWith('/')) {
-    // must start with a slash
-    return false;
-  }
-  pattern = pattern.substring(1);
-  var options = '';
-  if (pattern.endsWith('/')) {
-    pattern = pattern.substring(0, -1);
-  }
-  else if (pattern.endsWith('/i')) {
-    pattern = pattern.substring(0, -2);
-    options = 'i';
-  }
-  else {
-    // must end with a slash followed by zero or more options
-    return false;
-  }
-  return new RegExp(r"$pattern").hasMatch(value);//TODO
+	if (!pattern.startsWith('/')) {
+		// must start with a slash
+		return false;
+	}
+	pattern = pattern.substring(1);
+	var options = '';
+	if (pattern.endsWith('/')) {
+		pattern = pattern.substring(0, -1);
+	}
+	else if (pattern.endsWith('/i')) {
+		pattern = pattern.substring(0, -2);
+		options = 'i';
+	}
+	else {
+		// must end with a slash followed by zero or more options
+		return false;
+	}
+	return new RegExp(r"${pattern}").hasMatch(value);//TODO
 }
 
 
 class _TrusingNodeValidator implements NodeValidator {
-
-  @override
-  bool allowsAttribute(Element element, String attributeName, String value) =>
-      true;
-
-  @override
-  bool allowsElement(Element element) => true;
+	
+	@override
+	bool allowsAttribute(Element element, String attributeName, String value) => true;
+	
+	@override
+	bool allowsElement(Element element) => true;
 }
 
-  _TrusingNodeValidator _nodeValidator = new _TrusingNodeValidator();
+_TrusingNodeValidator _nodeValidator = new _TrusingNodeValidator();
